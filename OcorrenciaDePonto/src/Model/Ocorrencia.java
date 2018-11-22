46f97f921217d1320b923848ebda3f74b0191f18
pedro <pedro@LAPTOP-LDR84DP4>
2018-11-21 23:45:47 -0200

    Trabalhando com os apontamentos

--- OcorrenciaDePonto/src/Model/Ocorrencia.java
+++ OcorrenciaDePonto/src/Model/Ocorrencia.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class Ocorrencia {
 	private int id_FuncionarioAprovador;
 	private int id_FuncionarioSolicitante;
+	private int id_FuncionarioTrocaTurno;
 	private Date dataHora;
 	private String motivo;
 	private int tipo;
@@ -13,7 +14,16 @@ public class Ocorrencia {
 	private char status;
 	private String observacaoGestor;
 	private boolean abonado;
+	private int tipoSolicitacao;// 1-Folga extra, 2-Hora extra, 3-troca de plantão
 	
+	private Date chegadaAntecipada;
+	private Date chegadaAtrasada;
+	private Date ausenteMarcacao;
+	private Date saidaAntecipada;
+	private 
+	
+	//Criar ocorrências com erança, criando uma ocorrência mãe com caracteristicas comuns a todas as ocorrências, 
+	//e outras para cada tipo de ocorrência, tendo cada uma as suas caracteristicas adicionais e seus contrutores
 
 }
 	
