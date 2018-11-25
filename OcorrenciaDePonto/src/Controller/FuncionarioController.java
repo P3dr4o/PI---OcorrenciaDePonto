cac68de6c9f07d11514de0496e382965423af084
pedro <pedro@192.168.15.66>
2018-11-25 19:37:25 -0200

    organização projeto

--- OcorrenciaDePonto/src/Controller/FuncionarioController.java
+++ OcorrenciaDePonto/src/Controller/FuncionarioController.java
@@ -4,8 +4,9 @@ package Controller;
 public class FuncionarioController {
 	
 	public static void salvarFuncionário(int idFuncionario,String nome, int registro, int idcargo, int idsetor) {
-		//teste de atualização do eclipse
-		//teste de postagem do github
+		if(idFuncionario >0 && registro > 0 && idcargo > 0 && idsetor > 0) {
+			
+		}
 
 
 		
