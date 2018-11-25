18bf8f013301d546d000e3a8820021f058f3fc4a
pedro <pedro@192.168.15.66>
2018-11-25 19:06:16 -0200

    teste de postagem

--- OcorrenciaDePonto/src/Controller/FuncionarioController.java
+++ OcorrenciaDePonto/src/Controller/FuncionarioController.java
@@ -4,7 +4,7 @@ package Controller;
 public class FuncionarioController {
 	
 	public static void salvarFuncionário(int idFuncionario,String nome, int registro, int idcargo, int idsetor) {
-		
+		//teste de atualização do eclipse
 
 
 		
