d82eee104992038249c9263709d67a9297ef0373
pedro <pedro@192.168.15.66>
2018-11-25 18:50:02 -0200

    teste

--- OcorrenciaDePonto/src/Controller/FuncionarioController.java
+++ OcorrenciaDePonto/src/Controller/FuncionarioController.java
@@ -1,14 +1,10 @@
 package Controller;
 
-import org.omg.CORBA.IdentifierHelper;
-
-import Model.Cargo;
-import Model.Funcionario;
 
 public class FuncionarioController {
 	
 	public static void salvarFuncionário(int idFuncionario,String nome, int registro, int idcargo, int idsetor) {
-		//Funcionario f = new Funcionario(idFuncionario, nome, registro, idcargo, idsetor);
+		
 
 
 		
