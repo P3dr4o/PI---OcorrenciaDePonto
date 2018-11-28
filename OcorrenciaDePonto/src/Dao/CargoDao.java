f4f63f1ee4f7d9b3f3719cf6df785e3260481749
Usuário <Usurio@192.168.1.3>
2018-11-28 14:48:03 -0200

    Começo da camada Dao

---
+++ OcorrenciaDePonto/src/Dao/CargoDao.java
@@ -0,0 +1,124 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import Conexao.ConexaoBD;
+import Model.Cargo;
+
+public class CargoDao {
+	
+	private Connection con = null;
+	private PreparedStatement stmt = null;
+	private ResultSet rs = null;
+	
+	public CargoDao() {
+		ConexaoBD.getConnection();
+	}
+	
+	//metodo para selecionar todos os cargos
+	public List<Cargo> selectAllCargos() {
+		String sql = "SELECT * FROM cargo";
+		List<Cargo> listCargos = new ArrayList<>();
+		Cargo cargo = null;
+		
+		try {
+			stmt = con.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				cargo = new Cargo();
+				cargo.setIdCargo(rs.getInt("id_cargo"));
+				cargo.setNomeCargo(rs.getString("nome_cargo"));
+				listCargos.add(cargo);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		return listCargos;
+	}
+	
+	//metodo para selecionar cargo especifico pesquisando pelo nome
+	public Cargo selectCargo(String nomeCargo) {
+		String sql = "SELECT * FROM cargo WHERE id_cargo = ?";
+		Cargo cargo = null;
+		
+		try {
+			stmt = con.prepareStatement(sql);
+			stmt.setString(0, nomeCargo);
+			rs = stmt.executeQuery();
+			
+			if(rs.next()) {
+				cargo = new Cargo();
+				cargo.setIdCargo(rs.getInt("id_cargo"));
+				cargo.setNomeCargo(rs.getString("nome_cargo"));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return cargo;
+	}
+	
+	//metodo para criar novo cargo
+	public boolean createCargo(Cargo cargo) {
+		String sql = "INSERT INTO cargo VALUES(?, ?)";
+		
+		try {
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(0, gerarMaxID());
+			stmt.setString(1, cargo.getNomeCargo());
+			stmt.executeUpdate();
+			
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	//metodo para excluir um cargo
+	public boolean deleteCargo(Cargo cargo) {
+		String sql = "DELETE FROM cargo WHERE id_cargo = ?";
+		
+		try {
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(0, cargo.getIdCargo());
+			stmt.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	//metodo para retornar o maior ID possivel para ser inserido na tabela
+	public int gerarMaxID() {
+		String sql = "SELECT max(id) AS maior FROM cargo";
+		int max = 1;
+		try {
+			stmt = con.prepareStatement(sql);
+		    rs = stmt.executeQuery();
+		    if (rs.next()) {
+		    	max = rs.getInt("maior") + 1;
+		    }
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Erro ao buscar ID", "Erro", JOptionPane.ERROR_MESSAGE);
+		}
+		return max;
+	}
+
+}
--- OcorrenciaDePonto/src/Model/Cargo.java
+++ OcorrenciaDePonto/src/Model/Cargo.java
@@ -7,13 +7,14 @@ public class Cargo implements Serializable{
 	private int idCargo;
 	private String nomeCargo;
 	
+	public Cargo() {}
 	
 	public Cargo(int idCargo, String nomeCargo) {
 		if (nomeCargo != null && idCargo > 0) {
 			setNomeCargo(nomeCargo);
 			setIdCargo(idCargo);
 		}else {
-			//reportar exceção informando a existência de inconsistência nos dados repassados
+			//reportar exceï¿½ï¿½o informando a existï¿½ncia de inconsistï¿½ncia nos dados repassados
 		}
 		
 	}
@@ -23,9 +24,9 @@ public class Cargo implements Serializable{
 	}
 	
 	public void setIdCargo(int idCargo) {
-		/*Criar um código que chame uma consulta no banco de dados,
+		/*Criar um cï¿½digo que chame uma consulta no banco de dados,
 		para verificar qual o nome do cargo referente ao codigo passado
-		por parametro e logo depois modificar a variável nomeCargo
+		por parametro e logo depois modificar a variï¿½vel nomeCargo
 		com o valor retornado*/
 		this.idCargo = idCargo;
 	}
