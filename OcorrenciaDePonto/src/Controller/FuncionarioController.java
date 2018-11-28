32bed5afbe9541d209c2ab3f57a331d04f3ebef3
pedro <pedro@192.168.15.66>
2018-11-27 23:55:29 -0200

    atulizaçao

--- OcorrenciaDePonto/src/Controller/FuncionarioController.java
+++ OcorrenciaDePonto/src/Controller/FuncionarioController.java
@@ -1,15 +1,57 @@
 package Controller;
 
+import java.util.zip.DataFormatException;
+
+import Model.Funcionario;
+import Model.OcorreciaEvento;
+import Model.Ocorrencia;
+import Model.OcorrenciaFolgaExtra;
+import Model.OcorrenciaHoraExtra;
+import Model.OcorrenciaTrocaDePlantao;
+import Model.Setor;
 
 public class FuncionarioController {
-	
-	public static void salvarFuncionário(int idFuncionario,String nome, int registro, int idcargo, int idsetor) {
-		if(idFuncionario >0 && registro > 0 && idcargo > 0 && idsetor > 0) {
-			
+
+	public static void salvarFuncionário(Funcionario f) throws DataFormatException {
+		Funcionario funcionario = (Funcionario) Objetos.cloneSerializable(f);
+		// chamar uma função das classes em DAO para realizar a persitencia de dados no
+		// banco
+	}
+
+	public static void salvarSetor(Setor s) {
+		Setor setor = (Setor) Objetos.cloneSerializable(s);
+		// chamar uma função para realizar a persistencia
+	}
+
+	public static void slavarOcorrencia(Ocorrencia o) {
+		Ocorrencia ocorrencia = (Ocorrencia) Objetos.cloneSerializable(o);
+		String query = ocorrencia.getId() + " ," + ocorrencia.getTipo() + " ," + ocorrencia.getStatus() + " ,"
+				+ ocorrencia.getDataHora() + " ," + ocorrencia.getApontamento().getDataDoApontamento() + " ,"
+				+ ocorrencia.getFuncionarioSolicitante().getId_Funcionario() + " ,"
+				+ ocorrencia.getFuncionarioAprovador().getId_Funcionario() + " ," + ocorrencia.getMotivo() + " ,"
+				+ ocorrencia.getObservacaoGestor();
+
+		switch (ocorrencia.getTipo()) {
+		case 1:
+			OcorreciaEvento ocE = (OcorreciaEvento) ocorrencia;
+			query = query + " ," + ocE.getChegadaAntecipada() + " ," + ocE.getChegadaAtrasada() + " ,"
+					+ ocE.getAusenteMarcacao() + " ," + ocE.getSaidaAntecipada() + " ," + ocE.isNaoComparecimento();
+			break;
+		case 2:
+			OcorrenciaFolgaExtra ocF = (OcorrenciaFolgaExtra) ocorrencia;
+			query = query+" ,"+ocF.get
+			break;
+		case 3:
+			OcorrenciaHoraExtra ocH = (OcorrenciaHoraExtra) ocorrencia;
+			break;
+		case 4:
+			OcorrenciaTrocaDePlantao ocT = (OcorrenciaTrocaDePlantao) ocorrencia;
+
+			break;
+
+		default:
+			break;
 		}
-
-
-		
 	}
 
 }
---
+++ OcorrenciaDePonto/src/Controller/Start.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import View.TelaLogin;
+
+public class Start {
+
+	public static void main(String[] args) {
+		TelaLogin login = new TelaLogin();
+		
+		
+	}
+
+}
