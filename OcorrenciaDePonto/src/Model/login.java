926125a90e848144c39891ee56f51290de415c82
pedro <pedro@192.168.15.66>
2018-12-01 16:48:09 -0200

    nova classe login

---
+++ OcorrenciaDePonto/src/Model/login.java
@@ -0,0 +1,68 @@
+package Model;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class login {
+
+	private String usuario;
+	private String senha;
+	private Funcionario funcionario;
+
+	public login(String usuario, String senhaMD5) {
+		setUsuario(usuario);
+		this.senha = senhaMD5;
+	}
+	
+	
+	public boolean autentica(String usuario, String senha) {
+		String sen = getMD5(senha);
+		if(usuario.equals(this.usuario) && sen.equals(this.senha))
+			return true;
+		else
+			return false;
+	}
+	
+	
+	
+	public boolean mudarSenha(String usuario, String senhaAntiga, String senhaNova) {
+		String senN = getMD5(senhaNova);
+		if(autentica(usuario, senhaAntiga)) {
+			this.senha = senN;
+			
+			return true;
+		}
+		return false;
+	}
+
+	public static String getMD5(String senha) {
+		if (senha != null) {
+			String sen = "";
+			MessageDigest md = null;
+			try {
+				md = MessageDigest.getInstance("MD5");
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			BigInteger hash = new BigInteger(1, md.digest(senha.getBytes()));
+			sen = hash.toString(16);
+			return sen;
+		} else
+			return null;
+	}
+	
+	public boolean persistir() {
+		
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+
+}
